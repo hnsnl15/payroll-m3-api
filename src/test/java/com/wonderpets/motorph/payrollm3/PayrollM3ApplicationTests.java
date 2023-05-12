2adb0c6536762d424356d2fa4bb4bf9464c648f9
henson <hensonlao@gmail.com>
2023-05-12 20:47:19 +0800

    Initial commit

---
+++ src/test/java/com/wonderpets/motorph/payrollm3/PayrollM3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderpets.motorph.payrollm3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollM3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
