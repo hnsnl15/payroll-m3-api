eec9fd52835e24cdeca157af51f2a35c5119fb98
henson <hensonlao@gmail.com>
2023-05-14 18:27:54 +0800

    Modified EmployeeController.java to add delete controller

--- src/main/java/com/wonderpets/motorph/payrollm3/controller/EmployeeController.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/controller/EmployeeController.java
@@ -2,10 +2,8 @@ package com.wonderpets.motorph.payrollm3.controller;
 
 import com.wonderpets.motorph.payrollm3.model.Employee;
 import com.wonderpets.motorph.payrollm3.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +26,11 @@ public class EmployeeController {
     @GetMapping("/v1/employees/{id}")
     public Optional<Employee> retrieveEmployee(@PathVariable long id) {
         return employeeService.retrieveEmployee(id);
+    }
+
+    @DeleteMapping("/v1/employees/{id}")
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable long id) {
+        return employeeService.deleteEmployeeById(id);
     }
 
 }
