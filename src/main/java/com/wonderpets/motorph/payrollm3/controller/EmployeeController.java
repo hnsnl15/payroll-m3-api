b9c711b6181353182d5f055d770d3e9ca7ecdcf9
henson <hensonlao@gmail.com>
2023-05-14 17:41:52 +0800

    Created controller to get employee by id

--- src/main/java/com/wonderpets/motorph/payrollm3/controller/EmployeeController.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/controller/EmployeeController.java
@@ -3,10 +3,12 @@ package com.wonderpets.motorph.payrollm3.controller;
 import com.wonderpets.motorph.payrollm3.model.Employee;
 import com.wonderpets.motorph.payrollm3.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +23,11 @@ public class EmployeeController {
     @GetMapping("/v1/employees")
     public List<Employee> retrieveAllEmployee() {
         return employeeService.retrieveAllEmployee();
+    }
+
+    @GetMapping("/v1/employees/{id}")
+    public Optional<Employee> retrieveEmployee(@PathVariable long id) {
+        return employeeService.retrieveEmployee(id);
     }
 
 }
