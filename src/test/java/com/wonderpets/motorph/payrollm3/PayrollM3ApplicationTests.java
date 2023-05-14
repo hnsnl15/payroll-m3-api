1f91298289186a73a68ee1e201dbd25228124765
henson <hensonlao@gmail.com>
2023-05-14 17:19:22 +0800

    Updated PayrollM3ApplicationTests.java

--- src/test/java/com/wonderpets/motorph/payrollm3/PayrollM3ApplicationTests.java
+++ src/test/java/com/wonderpets/motorph/payrollm3/PayrollM3ApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class PayrollM3ApplicationTests {
 
     @Autowired
