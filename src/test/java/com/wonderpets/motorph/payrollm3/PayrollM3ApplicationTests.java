b64566e93ff03da407356fcc0899040b5a610e71
henson <hensonlao@gmail.com>
2023-05-14 11:50:29 +0800

    Updated PayrollM3ApplicationTests.java

--- src/test/java/com/wonderpets/motorph/payrollm3/PayrollM3ApplicationTests.java
+++ src/test/java/com/wonderpets/motorph/payrollm3/PayrollM3ApplicationTests.java
@@ -1,13 +1,29 @@
 package com.wonderpets.motorph.payrollm3;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PayrollM3ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private PayrollM3Application application;
+
+    @Test
+    void contextLoads() {
+        // Test if the application context loads successfully
+        Assertions.assertNotNull(application, "The application context failed to load.");
+    }
+
+    @Test
+    void applicationRunsSuccessfully() {
+        // Test if the application runs successfully
+        // Perform any assertions or verifications here
+
+        // Example: Check if the application instance is not null
+        Assertions.assertNotNull(application, "The application failed to start successfully.");
+    }
 
 }
