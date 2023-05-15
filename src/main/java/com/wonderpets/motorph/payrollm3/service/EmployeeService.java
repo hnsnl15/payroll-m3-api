8273a5dccb33284abc05143f8c2dd7a4a8b47cdb
henson <hensonlao@gmail.com>
2023-05-15 18:02:50 +0800

    Modified EmployeeService.java

--- src/main/java/com/wonderpets/motorph/payrollm3/service/EmployeeService.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/service/EmployeeService.java
@@ -4,12 +4,17 @@ import com.wonderpets.motorph.payrollm3.exception.UserAlreadyCreatedException;
 import com.wonderpets.motorph.payrollm3.exception.UserNotFoundException;
 import com.wonderpets.motorph.payrollm3.jpa.EmployeeRepository;
 import com.wonderpets.motorph.payrollm3.model.Employee;
+import com.wonderpets.motorph.payrollm3.model.Role;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.sql.DataSource;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -18,13 +23,34 @@ import java.util.Optional;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final DataSource dataSource;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, DataSource dataSource, BCryptPasswordEncoder passwordEncoder) {
         this.employeeRepository = employeeRepository;
+        this.dataSource = dataSource;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void userDetailsService(String username, String password) {
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        if (!jdbcUserDetailsManager.userExists(username)) {
+            User user = (User) User.withUsername(username)
+                    .password(password)
+                    .passwordEncoder(passwordEncoder::encode)
+                    .roles(Role.USER.toString())
+                    .build();
+            jdbcUserDetailsManager.createUser(user);
+        }
     }
 
     public List<Employee> retrieveAllEmployee() {
-        return this.employeeRepository.findAll();
+        List<Employee> employees = this.employeeRepository.findAll();
+        for (Employee employee : employees) {
+
+            userDetailsService(employee.getUsername(), employee.getPassword());
+        }
+        return employees;
     }
 
     public Optional<Employee> retrieveEmployee(long id) {
@@ -47,6 +73,12 @@ public class EmployeeService {
         if (employeeRepository.findByUsername(employee.getUsername()).isPresent()) {
             throw new UserAlreadyCreatedException("Username is not available.");
         }
+        if (employee.getPassword() == null) {
+            throw new IllegalArgumentException("Password cannot be null.");
+        }
+        String encodedPassword = passwordEncoder.encode(employee.getPassword());
+        userDetailsService(employee.getUsername(), encodedPassword);
+        employee.setPassword(encodedPassword);
         Employee createdEmployee = employeeRepository.save(employee);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
