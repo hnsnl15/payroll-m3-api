64ec142007de3c9c1f62969a90906d121463b396
henson <hensonlao@gmail.com>
2023-05-16 10:39:14 +0800

    Modified AuthControllerTest.java

--- src/test/java/com/wonderpets/motorph/payrollm3/controller/AuthControllerTest.java
+++ src/test/java/com/wonderpets/motorph/payrollm3/controller/AuthControllerTest.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static com.wonderpets.motorph.payrollm3.controller.EmployeeControllerTest.generateBasicAuthHeader;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -28,7 +26,6 @@ public class AuthControllerTest {
     private ResultActions mockPostOption(String urlTemplate, LoginForm userDetails) throws Exception {
         String requestBody = new ObjectMapper().writeValueAsString(userDetails);
         return mockMvc.perform(MockMvcRequestBuilders.post(urlTemplate)
-                .header(HttpHeaders.AUTHORIZATION, generateBasicAuthHeader("admin", "123"))
                 .content(requestBody)
                 .contentType(MediaType.APPLICATION_JSON)
         );
@@ -47,8 +44,8 @@ public class AuthControllerTest {
     }
 
     @Test
-    void testGetAuthToken() throws Exception {
-        mockPostOption("/auth-token", loginForm).andExpect(status().isOk());
+    void testGetAuthToken_WillPassedTest() throws Exception {
+        mockPostOption("/api/auth-token", loginForm).andExpect(status().isOk());
     }
 
 }
