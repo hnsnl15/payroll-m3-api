94a0584259738afb64100c8b2bdde5ef41b4311e
henson <hensonlao@gmail.com>
2023-05-17 16:32:07 +0800

    Updated EmployeeRepository.java

--- src/main/java/com/wonderpets/motorph/payrollm3/jpa/EmployeeRepository.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/jpa/EmployeeRepository.java
@@ -1,14 +1,14 @@
 package com.wonderpets.motorph.payrollm3.jpa;
 
-import com.wonderpets.motorph.payrollm3.model.Employee;
+import com.wonderpets.motorph.payrollm3.model.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findByUsername(String username);
+public interface EmployeeRepository extends JpaRepository<Employees, Long> {
+    Optional<Employees> findByUsername(String username);
 
     void deleteByUsername(String username);
 }
