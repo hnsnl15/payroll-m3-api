50ddee47f7d7993fa9ef563ac8e3b80cfeb01def
henson <hensonlao@gmail.com>
2023-05-20 11:00:11 +0800

    Updated InventoryController.java and InventoryService.java

--- src/main/java/com/wonderpets/motorph/payrollm3/controller/InventoryController.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/controller/InventoryController.java
@@ -1,4 +1,33 @@
 package com.wonderpets.motorph.payrollm3.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class InventoryController {
+
+    @GetMapping("/v1/inventory")
+    public void retrieveAllStock() {
+    }
+
+    @GetMapping("/v1/inventory/{engineNumber}")
+    public void retrieveStockByEngineNumber() {
+    }
+
+    @GetMapping("/v2/inventory")
+    public void retrieveStockByPagination() {
+    }
+
+    @PostMapping("/v1/inventory")
+    public void createStock() {
+    }
+
+    @PutMapping("/v1/inventory")
+    public void updateStock() {
+    }
+
+    @DeleteMapping("/v1/inventory")
+    public void deleteStock() {
+    }
+
 }
--- src/main/java/com/wonderpets/motorph/payrollm3/service/InventoryService.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/service/InventoryService.java
@@ -1,4 +1,29 @@
 package com.wonderpets.motorph.payrollm3.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class InventoryService {
+
+    public void createStock() {
+
+    }
+
+    public void retrieveAllStock() {
+    }
+
+    public void retrieveStockByEngineNumber() {
+    }
+
+    public void retrieveStocksByPagination() {
+    }
+
+    public void updateStock() {
+    }
+
+    public void deleteStock() {
+    }
+
 }
