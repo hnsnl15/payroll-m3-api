f8644c26d1bee88f86dde0d390349e4e6ec747cf
henson <hensonlao@gmail.com>
2023-05-20 10:12:00 +0800

    Modified InventoryControllerTest.java

--- src/main/java/com/wonderpets/motorph/payrollm3/jpa/InventoryJpaRepository.java
+++ src/main/java/com/wonderpets/motorph/payrollm3/jpa/InventoryJpaRepository.java
@@ -3,5 +3,8 @@ package com.wonderpets.motorph.payrollm3.jpa;
 import com.wonderpets.motorph.payrollm3.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryJpaRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByEngineNumber(String engineNumber);
 }
